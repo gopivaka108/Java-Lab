8d38e167429f24962e13b76f05dfb5af82c5b153
gopivaka108 <gopivaka108@gmail.com>
2025-11-17 23:33:37 +0530

    Implement ArraySumAverage to find min and max
    This program takes an array of integers as input and calculates the maximum and minimum values.

---
+++ exp-3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class  ArraySumAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no. of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int sum = 0;
+        System.out.println("Enter "+n+" numbers: ");
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+        int min = arr[0], max = arr[0];
+        for(int i=1;i<n;i++) {
+            if(arr[i]<min) min = arr[i];
+            if(arr[i]>max) max = arr[i];
+        }
+        System.out.println("Max: "+max);
+        System.out.println("Min: "+min);
+        sc.close();
+    }
+}
