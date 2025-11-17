c4c59b36d88c91b90021efc180fdcb5b161dada7
gopivaka108 <gopivaka108@gmail.com>
2025-11-17 23:06:16 +0530

    Add Student class with info display and grade calculation

---
+++ exp-1.java
@@ -0,0 +1,36 @@
+class Student {
+    String name;
+    int rollNumber;
+    double marks;
+    
+    void displayInfo() {
+        System.out.println("Name: "+name);
+        System.out.println("Roll Number: "+rollNumber);
+        System.out.println("Marks: "+marks);
+    }
+    
+    void calculateGrade() {
+        if(marks>=90) {
+            System.out.println("Grade A");   
+        }
+        else if(marks>=75 && marks<90) {
+            System.out.println("Grade B");
+        }
+        else if(marks>=50 && marks<75) {
+            System.out.println("Grade C");
+        }
+        else {
+            System.out.println("Fail");
+        }
+    }
+    
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Gopi";
+        s1.rollNumber = 93;
+        s1.marks = 100;
+
+        s1.displayInfo();
+        s1.calculateGrade();
+    }
+}
