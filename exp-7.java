27c149e7e9216798e26943c8aa8e480d3d09310e
gopivaka108 <gopivaka108@gmail.com>
2025-11-18 00:04:28 +0530

    Implement simple calculator in Java

---
+++ exp-7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+        System.out.println("Enter Operator  +,-,*,/ ");
+        char op = sc.next().charAt(0);
+        double result;
+        switch(op) {
+            case'+' : result = a+b; break;
+            case'-' : result = a-b; break;
+            case'*' : result = a*b; break;
+            case'/' : result = a/b; break;
+            default:
+                System.out.print("Invalid Operator");
+                sc.close();
+                return;
+        }
+        System.out.print("Result: "+result);
+        sc.close();
+    }
+}
